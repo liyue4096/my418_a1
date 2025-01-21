7388e6ad2d672ecdbad7403dc1b83bc2d7c50e3b
liyue4096 <leon2025@LeonROG.>
2025-01-20 21:59:04 -0500

    a1 final

--- prog5_saxpy/saxpyStreaming.cpp
+++ prog5_saxpy/saxpyStreaming.cpp
@@ -4,11 +4,10 @@
 #include <stdint.h>
 
 extern void saxpySerial(int N,
-			float scale,
-			float X[],
-			float Y[],
-			float result[]);
-
+                        float scale,
+                        float X[],
+                        float Y[],
+                        float result[]);
 
 void saxpyStreaming(int N,
                     float scale,
@@ -17,6 +16,18 @@ void saxpyStreaming(int N,
                     float result[])
 {
     // Replace this code with ones that make use of the streaming instructions
-    saxpySerial(N, scale, X, Y, result);
-}
 
+    size_t num_iterations = N / 4; // 4 integers fit into a 128-bit register
+
+    __m128 scale_vec = _mm_set1_ps(scale);
+
+    for (size_t i = 0; i < num_iterations; ++i)
+    {
+        __m128 data_x = _mm_load_ss(&X[4 * i]);
+        __m128 data_y = _mm_load_ss(&Y[4 * i]);
+
+        __m128 data_z = _mm_add_ps(_mm_mul_ps(scale_vec, data_x), data_y);
+
+        _mm_stream_ps(&result[4 * i], data_z);
+    }
+}
